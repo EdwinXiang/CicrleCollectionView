a32def60715804eb1d24c367fdcd6000e4427b15
EdwinXiang <306229443@qq.com>
2019-06-01 15:22:18 +0800

    project

---
+++ CircleCollectionViewTests/CircleCollectionViewTests.m
@@ -0,0 +1,37 @@
+//
+//  CircleCollectionViewTests.m
+//  CircleCollectionViewTests
+//
+//  Created by 向伟 on 2019/6/1.
+//  Copyright © 2019 xinyue. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CircleCollectionViewTests : XCTestCase
+
+@end
+
+@implementation CircleCollectionViewTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
